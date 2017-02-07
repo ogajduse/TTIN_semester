c75eade9b55a33e38bdd0b1514357be58f814b05
Ondrej Gajdusek <ogajduse@redhat.com>
2017-02-07 14:18:33 +0100

    added class adresar

---
+++ src/cz/vutbr/feec/adresare/Adresar.java
@@ -0,0 +1,12 @@
+package cz.vutbr.feec.adresare;
+
+public class Adresar {
+
+    public String nazev;
+    public Adresar levy;
+    public Adresar pravy;
+
+    public Adresar(String nazev) {
+        this.nazev = nazev;
+    }
+}
---
+++ src/cz/vutbr/feec/adresare/Spustitelna.java
@@ -0,0 +1,16 @@
+package cz.vutbr.feec.adresare;
+
+public class Spustitelna {
+
+    public static void main(String[] args) {
+        Adresar a1 = new Adresar("a1");
+        Adresar a4 = new Adresar("a4");
+        a1.levy = a1;
+        Adresar a2 = new Adresar("a2");
+        a1.pravy = a2;
+        Adresar a3 = new Adresar("a3");
+        a2.pravy = a3;
+        Adresar a5 = new Adresar("a5");
+        a2.levy = a5;
+    }
+}
