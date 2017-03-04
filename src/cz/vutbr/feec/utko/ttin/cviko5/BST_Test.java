4ab6e5470046093817340fc2dd1624e04275e4c3
Ondrej Gajdusek <ogajduse@redhat.com>
2017-03-04 16:57:55 +0100

    initalize classes for 5th week

---
+++ src/cz/vutbr/feec/utko/ttin/cviko5/BST_Test.java
@@ -0,0 +1,10 @@
+package cz.vutbr.feec.utko.ttin.cviko5;
+
+/**
+ * Created by ogajduse on 3/4/17.
+ */
+public class BST_Test {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/cz/vutbr/feec/utko/ttin/cviko5/BinarySearchTree.java
@@ -0,0 +1,7 @@
+package cz.vutbr.feec.utko.ttin.cviko5;
+
+/**
+ * Created by ogajduse on 3/4/17.
+ */
+public class BinarySearchTree {
+}
