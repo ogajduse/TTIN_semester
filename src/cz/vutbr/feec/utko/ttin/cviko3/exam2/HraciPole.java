458249fe4cb4ee83d331c29332b908674e3c6657
Ondrej Gajdusek <ogajduse@redhat.com>
2017-03-21 14:50:32 +0100

    added method klonujAPohni, optimized method posun

--- src/cz/vutbr/feec/utko/ttin/cviko3/exam2/HraciPole.java
+++ src/cz/vutbr/feec/utko/ttin/cviko3/exam2/HraciPole.java
@@ -1,5 +1,7 @@
 package cz.vutbr.feec.utko.ttin.cviko3.exam2;
 
+import java.util.ArrayList;
+
 /**
  * Created by ogajduse on 2/21/17.
  */
@@ -7,36 +9,69 @@ public class HraciPole {
     private int[][] pole = new int[][]{{4, 1, 2}, {3, 0, 5}, {6, 7, 8}};
     private int prazdneX;
     private int prazdneY;
+    private ArrayList<Integer> pohyby = new ArrayList<Integer>();
 
     public static final int NAHORU = 0;
     public static final int DOLU = 1;
     public static final int DOLEVA = 2;
     public static final int DOPRAVA = 3;
 
-    public void posun(int i) {
+    public HraciPole klonujAPohni(int smer) {
+        HraciPole nove = new HraciPole();
+        nove.prazdneX = this.prazdneX;
+        nove.prazdneY = this.prazdneY;
+        nove.pohyby.addAll(this.pohyby);
+        nove.pohyby.add(smer);
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                nove.pole[x][y] = this.pole[x][y];
+            }
+        }
+
+        boolean jeMozny = nove.posun(smer);
+        if (jeMozny) {
+            return nove;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean posun(int i) {
         switch (i) {
             case NAHORU:
-                pole[prazdneX][prazdneY] = pole[prazdneX - 1][prazdneY];
-                pole[prazdneX + -1][prazdneY] = 0;
-                prazdneX--;
-                break;
-            case DOLU:
-                pole[prazdneX][prazdneY] = pole[prazdneX + 1][prazdneY];
-                pole[prazdneX + 1][prazdneY] = 0;
-                prazdneX++;
-                break;
-            case DOLEVA:
+                if (prazdneY < 1) {
+                    return false;
+                }
                 pole[prazdneX][prazdneY] = pole[prazdneX][prazdneY - 1];
                 pole[prazdneX][prazdneY - 1] = 0;
                 prazdneY--;
-                break;
-            case DOPRAVA:
+                return true;
+            case DOLU:
+                if (prazdneY > 1) {
+                    return false;
+                }
                 pole[prazdneX][prazdneY] = pole[prazdneX][prazdneY + 1];
                 pole[prazdneX][prazdneY + 1] = 0;
                 prazdneY++;
-                break;
+                return true;
+            case DOLEVA:
+                if (prazdneX < 1) {
+                    return false;
+                }
+                pole[prazdneX][prazdneY] = pole[prazdneX - 1][prazdneY];
+                pole[prazdneX - 1][prazdneY] = 0;
+                prazdneX--;
+                return true;
+            case DOPRAVA:
+                if (prazdneX > 1) {
+                    return false;
+                }
+                pole[prazdneX][prazdneY] = pole[prazdneX + 1][prazdneY];
+                pole[prazdneX + 1][prazdneY] = 0;
+                prazdneX++;
+                return true;
             default:
-                break;
+                return false;
 
         }
     }
@@ -52,9 +87,9 @@ public class HraciPole {
         return text;
     }
 
-    public boolean hashCode(HraciPole obj){
+    public boolean hashCode(HraciPole obj) {
         for (int i = 0; i < 3; i++) {
-            
+
         }
         return false;
     }
@@ -64,7 +99,7 @@ public class HraciPole {
     }
 
     public boolean jeReseni() {
-        int[][] reseni = { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, -1 } };
+        int[][] reseni = {{1, 4, 7}, {2, 5, 8}, {3, 6, 0}};
         for (int x = 0; x < reseni.length; x++) {
             for (int y = 0; y < reseni[0].length; y++) {
                 if (pole[x][y] != reseni[x][y]) {
