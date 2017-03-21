ee8bd234ee72482463c400a5ff56c33fab07ba71
Ondrej Gajdusek <ogajduse@redhat.com>
2017-03-21 13:47:53 +0100

    raw import from elearning

---
+++ src/cz/vutbr/feec/utko/ttin/cviko7/elearningImport/example/P10BFS.java
@@ -0,0 +1,11 @@
+package cz.vutbr.feec.cisla;
+
+public class P10BFS {
+	public static void main(String[] args) {
+		HraciPole h = null;
+		// TODO: vytvorit nahodne hraci pole (viz priklad P2Randomize)
+		BFS bfs = new BFS();
+		bfs.vypisTahy(h);
+		
+	}
+}
