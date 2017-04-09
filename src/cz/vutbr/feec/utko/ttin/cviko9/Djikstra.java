ca36963caf39dfa9964633025ac13ef57172602d
Ondrej Gajdusek <ogajduse@redhat.com>
2017-04-09 09:47:41 +0200

    scratch version of Djikstra

--- src/cz/vutbr/feec/utko/ttin/cviko9/Djikstra.java
+++ src/cz/vutbr/feec/utko/ttin/cviko9/Djikstra.java
@@ -1,7 +1,19 @@
 package cz.vutbr.feec.utko.ttin.cviko9;
 
+import cz.vutbr.feec.utko.ttin.cviko6.Cesta;
+import cz.vutbr.feec.utko.ttin.cviko6.Graf;
+
 /**
  * Created by ogajduse on 4/4/17.
  */
 public class Djikstra {
+    Graf g;
+    public Djikstra(Graf g) {
+        this.g = g;
+    }
+
+    public Cesta run(String from, String to) {
+
+        return null;
+    }
 }
