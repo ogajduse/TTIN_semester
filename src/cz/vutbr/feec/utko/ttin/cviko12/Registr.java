2bf5a4a295f49de1384004ee76d62d103932397d
Ondrej Gajdusek <ogajduse@redhat.com>
2017-05-02 10:53:45 +0200

    initalize week12 files

---
+++ src/cz/vutbr/feec/utko/ttin/cviko12/Registr.java
@@ -0,0 +1,7 @@
+package cz.vutbr.feec.utko.ttin.cviko12;
+
+/**
+ * Created by ogajduse on 4/25/17.
+ */
+public class Registr {
+}
---
+++ src/cz/vutbr/feec/utko/ttin/cviko12/sklad/Executable.java
@@ -0,0 +1,7 @@
+package cz.vutbr.feec.utko.ttin.cviko12.sklad;
+
+/**
+ * Created by ogajduse on 4/25/17.
+ */
+public class Executable {
+}
